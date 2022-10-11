389dc6ec693a3e815f52734fe7865a9bcf7aa67e
risangdevs <risangdevs@gmail.com>
2022-10-11 11:55:58 +0700

    detail:created stats section & list of owned token section

--- mobile/src/screens/Detail.jsx
+++ mobile/src/screens/Detail.jsx
@@ -5,7 +5,10 @@ import {Image, ImageBackground, ScrollView, Text, View} from 'react-native';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import {VictoryLine, VictoryChart} from 'victory-native';
 
+import Dimension from './../utils/Dimension';
+const height = Dimension.responsiveHeight();
 export default function Detail({route, navigation}) {
   const {name, token} = route.params;
   const [detail, setDetail] = useState({});
@@ -40,10 +43,13 @@ export default function Detail({route, navigation}) {
       console.log(error);
     }
   };
+
   return (
     <View style={{top: top}}>
       <Header title={name} />
-      <ScrollView>
+      <ScrollView
+        className="overflow-y-scroll mt-4"
+        style={{height: height - 220}}>
         <View className="m-2">
           <ImageBackground
             className="w-full h-40 justify-end items-center"
@@ -71,8 +77,45 @@ export default function Detail({route, navigation}) {
             </View>
           </View>
         </View>
-        <View className="m-2">
-
+        <View className="m-2 bg-slate-100 rounded-2xl">
+          <Text className="font-bold text-2xl mx-auto text-emerald-900">
+            Stats
+          </Text>
+          <VictoryChart>
+            <VictoryLine
+              style={{
+                data: {
+                  stroke: '#c43a31',
+                  strokeWidth: 1,
+                },
+                labels: {
+                  fontSize: 15,
+                  fill: ({datum}) => (datum.x === 3 ? '#000000' : '#c43a31'),
+                },
+              }}
+              data={stat.map(e => {
+                return {x: e.timestamp, y: e.floor_price_eth};
+              })}
+            />
+          </VictoryChart>
+        </View>
+        <View className="m-2 bg-slate-200 rounded-2xl py-4">
+          {tokens.map((f, j) => {
+            return (
+              <View key={j}>
+                <View className="flex flex-row justify-center my-1 p-1 items-center">
+                  <Image
+                    className="w-10 h-10 mx-2"
+                    source={{uri: f.collection.image_url}}
+                  />
+                  <View>
+                    <Text>tokenID: {f.token_id}</Text>
+                    <Text>total volume: {f.collection.total_volume}</Text>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
         </View>
       </ScrollView>
       <Footer navigation={navigation} />
